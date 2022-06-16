ae39fca71f986b46eac39f94582da023ed0aeaef
Ezequiel Oliveira <ezequieloliveirasg@gmail.com>
2022-06-16 17:44:02 -0300

    dont query usync devices if list is empty

--- src/Socket/messages-send.ts
+++ src/Socket/messages-send.ts
@@ -159,6 +159,10 @@ export const makeMessagesSocket = (config: SocketConfig) => {
 			}
 		}
 
+		if (!users.length) {
+			return deviceResults
+		}
+
 		const iq: BinaryNode = {
 			tag: 'iq',
 			attrs: {
