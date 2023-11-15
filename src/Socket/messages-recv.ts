b4acbeb83979250ceb1fd53b18e0734a345dcaae
Ezequiel Oliveira <ezequieloliveirasg@gmail.com>
2023-11-15 11:05:36 -0300

    ajustes

--- src/Socket/messages-recv.ts
+++ src/Socket/messages-recv.ts
@@ -497,7 +497,7 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 						if(willSendMessageAgain(ids[0], key.participant)) {
 							// if(key.fromMe) {
 								try {
-									logger.debug({ attrs, key }, 'recv retry request')
+									logger.info({ attrs, key }, 'recv retry request')
 									await sendMessagesAgain(key, ids, retryNode!)
 								} catch(error) {
 									let log = true;
