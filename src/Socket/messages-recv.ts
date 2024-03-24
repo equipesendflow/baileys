8e9aeb60e5d68ed8e9ce1778995147f86dd9f12a
Ezequiel Oliveira <ezequieloliveirasg@gmail.com>
2024-03-24 14:18:30 -0300

    fix missing messages

--- src/Socket/messages-recv.ts
+++ src/Socket/messages-recv.ts
@@ -51,7 +51,7 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 
 	let sendActiveReceipts = false
 
-	const sendMessageAck = async({ tag, attrs }: BinaryNode) => {
+	const sendMessageAck = async({ tag, attrs, content }: BinaryNode) => {
 		const stanza: BinaryNode = {
 			tag: 'ack',
 			attrs: {
@@ -69,11 +69,11 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 			stanza.attrs.recipient = attrs.recipient
 		}
 
-		if(!!attrs.type) {
+		if(!!attrs.type && (tag !== 'message' || getBinaryNodeChild({ tag, attrs, content }, 'unavailable'))) {
 			stanza.attrs.type = attrs.type
 		}
 
-		if(tag === "message") {
+		if(tag === 'message' && getBinaryNodeChild({ tag, attrs, content }, 'unavailable')) {
 			stanza.attrs.from = authState.creds.me!.id
 		}
 
