433300ba4dafa7a4682467dad3fc53b86c6972f7
Ezequiel Oliveira <ezequieloliveirasg@gmail.com>
2024-03-24 14:28:53 -0300

    Support for receiving "view once" audio messages

--- src/Utils/messages.ts
+++ src/Utils/messages.ts
@@ -671,6 +671,7 @@ export const normalizeMessageContent = (content: WAMessageContent | null | undef
 			 || message?.viewOnceMessage
 			 || message?.documentWithCaptionMessage
 			 || message?.viewOnceMessageV2
+			 || message?.viewOnceMessageV2Extension
 			 || message?.editedMessage
 		 )
 	 }
